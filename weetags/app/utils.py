69beb3c05f4e78e7335b048518231c97f472da59
morague <rom88.viry@gmail.com>
2024-07-28 00:01:18 +0200

    docs(app.utils): typing

--- weetags/app/utils.py
+++ weetags/app/utils.py
@@ -11,30 +11,17 @@ TableName = FieldName = str
 
 CHARS = ascii_lowercase + ascii_uppercase + digits
 
-def infer_loader(path: str) -> str:
+def infer_loader(path: str) -> JlLoader | JsonLoader:
     ext = path.split(".")[-1]
     loaders = {
         "json": JsonLoader,
         "jl": JlLoader,
         "jsonlines": JlLoader
-        
     }
     loader = loaders.get(ext, None)
     if loader is None:
         raise ValueError("non recognized file type")
     return loader
-    
-def map_path_to_callback(path: str):
-    pass
-
-
-setting = {
-    "endpoint": "show/<tree_name>",
-    "tree_name": "*"
-}
-
-
-
 
 def generate_uuid() -> str:
     """fewer collisions method"""
