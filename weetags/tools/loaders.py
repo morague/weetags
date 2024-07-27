ba4169cdc8f2a1e1fda655c0eb544b8f41ae92f9
morague <rom88.viry@gmail.com>
2024-07-27 23:59:29 +0200

    docs(tools.loaders): typing

--- weetags/tools/loaders.py
+++ weetags/tools/loaders.py
@@ -16,7 +16,7 @@ class Loader(object):
     def default_loader(self) -> Generator:
         for line in iter(self.data):
             yield line
-    
+
 
 class JsonLoader(Loader):
     def __init__(self, fp: str, strategy: str= "default") -> None:
@@ -26,16 +26,16 @@ class JsonLoader(Loader):
             "lazy": self.lazy_loader
         }[strategy]
 
-    def default_loader(self):
+    def default_loader(self) -> Generator:
         with open(self.fp) as f:
             data = iter(json.load(f))
             while line := next(data, None):
                 yield line
-                
-    def lazy_loader(self):
+
+    def lazy_loader(self) -> Generator:
         raise NotImplemented()
-         
-         
+
+
 class JlLoader(Loader):
     def __init__(self, fp: str, strategy: str= "default") -> None:
         self.fp = fp
@@ -44,16 +44,16 @@ class JlLoader(Loader):
             "lazy": self.lazy_loader
         }[strategy]
 
-    def default_loader(self):
+    def default_loader(self) -> Generator:
         with open(self.fp) as f:
             data = iter([json.loads(line) for line in f.readlines()])
             while line := next(data, None):
                 yield line
 
-    def lazy_loader(self):
+    def lazy_loader(self) -> Generator:
         with open(self.fp) as f:
             while line := f.readline():
                 yield json.loads(line.strip("\n"))
-    
+
     def __call__(self) -> Any:
         yield from self.loader()
